0df2d2712655c45e8da8f9daefa5807fb058f8f8
gabrieljimenezc01 <jesusgabrieljimenezceron@gmail.com>
2024-10-02 16:12:30 -0500

    modificacion controladores 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaMascotasComponent } from './lista-mascotas/lista-mascotas.component';
 import { EditarMascotasComponent } from './editar-mascotas/editar-mascotas.component';
+import { VistaMascotaComponent } from './vista-mascota/vista-mascota.component';
 
 const routes: Routes = [
  {path: 'mascotas', component: ListaMascotasComponent},
  {path: 'mascotas/editar/:idMascota', component: EditarMascotasComponent},
  {path: 'mascotas/agregar',component: EditarMascotasComponent},
+ {path: 'mascotas/vista/:idMascota',component: VistaMascotaComponent},
  {path: '**',redirectTo: '/mascotas',pathMatch:'full'}
 ];
 
--- src/app/vista-mascota/vista-mascota.component.ts
+++ src/app/vista-mascota/vista-mascota.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MascotaService } from '../shared/mascota.service';
+import { MascotaModel } from '../shared/mascota.model';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-vista-mascota',
   templateUrl: './vista-mascota.component.html',
   styleUrl: './vista-mascota.component.css'
 })
-export class VistaMascotaComponent {
+export class VistaMascotaComponent implements OnInit{
+
+  idMascota= '';
+  mascota=new MascotaModel('','','','','','','','');
+  constructor(private mascotaService: MascotaService,private route: ActivatedRoute,private router: Router){
+  }
+
+  ngOnInit(){
+    this.idMascota=this.route.snapshot.params['idMascota'];
+    console.log(`El idMascota es ${this.idMascota}`);
+
+     
+     }
+
+  }
 
 }
